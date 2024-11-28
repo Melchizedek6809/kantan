a50936d15073fcd55f8ff29c2322e5660f9ea65b
Ben (MBA M2) <ben@wolkenwelten.net>
2024-11-28 17:14:10 +0100

    Fixed formatting

--- src/lib/input/inputContext.ts
+++ src/lib/input/inputContext.ts
@@ -1,28 +1,28 @@
 import type { Game } from "../game";
 
 export class InputContext {
-  private pressedKeys: Set<string>;
+	private pressedKeys: Set<string>;
 
-  constructor(public readonly game: Game) {
-    this.pressedKeys = new Set<string>();
-    this.addEventListeners();
-  }
+	constructor(public readonly game: Game) {
+		this.pressedKeys = new Set<string>();
+		this.addEventListeners();
+	}
 
-  private addEventListeners() {
-    const canvas = this.game.canvas;
-    canvas.addEventListener("keydown", this.handleKeyDown.bind(this));
-    canvas.addEventListener("keyup", this.handleKeyUp.bind(this));
-  }
+	private addEventListeners() {
+		const canvas = this.game.canvas;
+		canvas.addEventListener("keydown", this.handleKeyDown.bind(this));
+		canvas.addEventListener("keyup", this.handleKeyUp.bind(this));
+	}
 
-  private handleKeyDown(event: KeyboardEvent) {
-    this.pressedKeys.add(event.key);
-  }
+	private handleKeyDown(event: KeyboardEvent) {
+		this.pressedKeys.add(event.key);
+	}
 
-  private handleKeyUp(event: KeyboardEvent) {
-    this.pressedKeys.delete(event.key);
-  }
+	private handleKeyUp(event: KeyboardEvent) {
+		this.pressedKeys.delete(event.key);
+	}
 
-  public isPressed(key: string): boolean {
-    return this.pressedKeys.has(key);
-  }
+	public isPressed(key: string): boolean {
+		return this.pressedKeys.has(key);
+	}
 }
